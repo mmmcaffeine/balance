b911434a590337e9b728926250e2d068e7c58b2c
Mark Roberts <mroberts@geodesys.com>
2021-10-15 00:22:19 +0100

    Made StringReplacementBalancedCalculator more efficient by not creating multiple instances of the same regex
    We now create the `Regex` once, then pass that through to an overload of `IsBalanced`. Now it doesn't matter how many times we go through the recursive function call we will only ever have one `Regex`.

--- src/Balance/StringReplacementBalancedCalculator.cs
+++ src/Balance/StringReplacementBalancedCalculator.cs
@@ -6,8 +6,6 @@ namespace Dgt.Balance
 {
     public class StringReplacementBalancedCalculator : IBalancedCalculator
     {
-        // TODO Prevent needless creation of the same regex if we call this recursively
-        // TODO Fix multiple enumerations of delimiters
         public bool IsBalanced(string input, IEnumerable<Delimiter> delimiters)
         {
             var strings = delimiters.Select(x => x.ToString());
@@ -15,12 +13,17 @@ namespace Dgt.Balance
             var pattern = string.Join('|', escapedStrings);
             var regex = new Regex(pattern, RegexOptions.Compiled);
 
+            return IsBalanced(input, regex);
+        }
+
+        private bool IsBalanced(string input, Regex regex)
+        {
             var value = regex.Replace(input, string.Empty);
 
             if (value == string.Empty) return true;
             if (value == input) return false;
 
-            return IsBalanced(value, delimiters);
+            return IsBalanced(value, regex);
         }
     }
 }
