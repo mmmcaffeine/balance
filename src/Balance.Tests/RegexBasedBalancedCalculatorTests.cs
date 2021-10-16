8ce4b22435e9ba8856c4ee2472ec555a97fd729b
Mark Roberts <mroberts@geodesys.com>
2021-10-16 12:25:33 +0100

    Added exploratory tests around use of zero-width negative lookahead assertions
    I don't think it is possible to use a negated character group to prevent matching nested delimiters any more. This is because we can no longer guarantee the delimiter will only be a single character.
    
    It appears we can still use alternation but with zero-width negative lookahead assertions to check for nested delimiters.
    
    These tests do not check `RegexBasedBalancedCalculator` per se, but do allow me to explore what regex pattern I will need that type to build.

--- src/Balance.Tests/RegexBasedBalancedCalculatorTests.cs
+++ src/Balance.Tests/RegexBasedBalancedCalculatorTests.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Dgt.Balance
 {
     public class RegexBasedBalancedCalculatorTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public RegexBasedBalancedCalculatorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Theory]
         [ClassData(typeof(DelimitersOnlyTestData))]
         [ClassData(typeof(DelimitedStringsTestData))]
@@ -21,6 +30,26 @@ namespace Dgt.Balance
         
             // Assert
             isBalanced.Should().Be(expectedIsBalanced);
+        }
+
+        [Theory]
+        [InlineData(@"<%-- ASPX Comment --%>",      @"<%-- ASPX Comment --%>", true)]  // Simple case
+        [InlineData(@"<%-- <%-- ASPX Comment --%>", @"<%-- ASPX Comment --%>", true)]  // Should match and leave the unbalanced starting delimiter
+        [InlineData(@"<%-- ASPX Comment --%> --%>", @"<%-- ASPX Comment --%>", true)]  // Should match and leave the unbalanced ending delimiter
+        [InlineData(@"ASPX Comment --%>",           "",                        false)] // No starting delimiter
+        [InlineData(@"<%-- ASPX Comment",           "",                        false)] // No ending delimiter
+        [InlineData(@"<%--/*--%>",                  "",                        false)] // Contains starting multiline comment delimiter
+        [InlineData(@"<%--*/--%>",                  "",                        false)] // Contains ending multiline comment delimiter
+        public void NegativeLookAhead_Should_FilterNestedMultiCharacterDelimiters
+            (string input, string expectedValue, bool expectedSuccess)
+        {
+            const string pattern = @"<%--((?!(/\*|\*/|<%--|--%>)).)*?--%>";
+            var match = Regex.Match(input, pattern);
+            
+            _output.WriteLine(match.Value);
+
+            match.Success.Should().Be(expectedSuccess);
+            match.Value.Should().Be(expectedValue);
         }
     }
 }
