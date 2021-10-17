440f38bca7286ebc5c443d4429317e07c003ed9e
Mark Roberts <mroberts@geodesys.com>
2021-10-17 11:08:07 +0100

    Added method to find _all_ indices of a given substring in a string
    These tests are currently in `PairedIndicesBalancedCalculatorTests` even though they are not testing `PairedIndicesBalancedCalculator`. They are, however, testing parts of an algorithm we intend to "lift and shift" into it.
    
    This is intended to be used as part of a new implementation of `IBalancedCalculator`. The idea is we will find all indices of the delimiters and then pair  up the starts and ends. We will then look for any pairs that don't overlap with any other pairs and remove them. We will repeat this until no more pairs can be removed. If there are no pairs left the input is balanced. If there are any pairs left the input is not balanced.

---
+++ src/Balance.Tests/PairedIndicesBalancedCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Balance
+{
+    public class PairedIndicesBalancedCalculatorTests
+    {
+        [Theory]
+        [MemberData(nameof(GetIndicesOfTestData))]
+        public void IndicesOf_Should_ReturnIndicesOfSubstring(string value, string substring, IEnumerable<int> expectedIndices)
+        {
+            IndicesOf(value, substring).Should().Equal(expectedIndices);
+        }
+        
+        private static TheoryData<string, string, IEnumerable<int>> GetIndicesOfTestData()
+        {
+            return new TheoryData<string, string, IEnumerable<int>>
+            {
+                { "No parens", ")", Array.Empty<int>() },
+                { "()", ")", new[] { 1 } },
+                { "ABC (()) DEF ()", "(", new[] { 4, 5, 13 } },
+                { "ABC (()) ABC (())", "ABC", new[] { 0, 9 } }
+            };
+        }
+
+        private static IEnumerable<int> IndicesOf(string value, string substring)
+        {
+            var indices = new List<int>();
+            var index = 0;
+
+            while ((index = value.IndexOf(substring, index, StringComparison.Ordinal)) != -1)
+            {
+                indices.Add(index++);
+            }
+
+            return indices;
+        }
+    }
+}
---
+++ src/Balance/PairedIndicesBalancedCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Dgt.Balance
+{
+    public class PairedIndicesBalancedCalculator : IBalancedCalculator
+    {
+        public bool IsBalanced(string input, IEnumerable<Delimiter> delimiters)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
