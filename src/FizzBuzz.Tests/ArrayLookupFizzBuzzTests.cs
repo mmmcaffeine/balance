fcde53e2afeea6ceffe136c76782d4924b0dd864
Mark Roberts <mroberts@geodesys.com>
2021-10-18 23:25:55 +0100

    Added implementation that takes advantage of the fact the pattern of answers repeats every 15 items
    As the pattern repeats we can get away with storing a small lookup of how to get the answer for values in the range 0..14, and then figure out where in the repeating pattern the value lies. Armed with that information we can then get a function to calculate the answer for us.  We can't store the _actual_ answer because we'd end up returning e.g. 1 for an input of 16!

---
+++ src/FizzBuzz.Tests/ArrayLookupFizzBuzzTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Dojo.FizzBuzz
+{
+    public class ArrayLookupFizzBuzzTests
+    {
+        [Theory]
+        [ClassData(typeof(ClassicTestData))]
+        public void GetFizzBuzz(int value, string expected)
+        {
+            ArrayLookupFizzBuzz.GetFizzBuzz(value).Should().Be(expected);
+        }
+    }
+}
---
+++ src/FizzBuzz/ArrayLookupFizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dgt.Dojo.FizzBuzz
+{
+    public static class ArrayLookupFizzBuzz
+    {
+        private static readonly Func<int, string>[] AnswerFuncs =
+        {
+            i => i.ToString(),
+            i => i.ToString(),
+            _ => "Fizz",
+            i => i.ToString(),
+            _ => "Buzz",
+            _ => "Fizz",
+            i => i.ToString(),
+            i => i.ToString(),
+            _ => "Fizz",
+            _ => "Buzz",
+            i => i.ToString(),
+            _ => "Fizz",
+            i => i.ToString(),
+            i => i.ToString(),
+            _ => "FizzBuzz"
+        };
+
+        public static string GetFizzBuzz(int value)
+        {
+            var index = value - 1;
+            
+            while (index >= 15)
+            {
+                index -= 15;
+            }
+
+            return AnswerFuncs[index](value);
+        }
+    }
+}
