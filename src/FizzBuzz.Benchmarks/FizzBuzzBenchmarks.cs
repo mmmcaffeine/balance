446459e7e02644070afd9099ebae7c4a74af548a
Mark Roberts <mroberts@geodesys.com>
2021-10-19 08:30:28 +0100

    Added very basic benchmarks for the six different FizzBuzz implementations we currently have
    We're using BenchmarkDotNet for the purposes of benchmarking, rather than trying to roll our own solution using something like a `Stopwatch`. We don't yet know _much_ about the library in question, but we are at least seeing repeatable results.
    
    I'm surprised the versions using tuples with the modulo operator execute faster than the version with `if` statements and the modulo operator.
    
    I'm _very_ surprised the version looking up a function from an array is faster than the classic version.
    
    I'm _not_ surprised looking an `int` up in a `Hashset<int>` is faster than an `int[]` but I _am_ surprised by just how much slower the array solution is!
    
    This _very_ basic test was a good illustration of why you check your assumptions before optimising!

---
+++ src/FizzBuzz.Benchmarks/FizzBuzzBenchmarks.cs
@@ -0,0 +1,68 @@
+﻿// See following link for CA1822. However, BenchmarkDotNet requires benchmarks to be instance methods
+// https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1822
+#pragma warning disable CA1822
+
+using BenchmarkDotNet.Attributes;
+
+namespace Dgt.Dojo.FizzBuzz
+{
+    [MemoryDiagnoser]
+    public class FizzBuzzBenchmarks
+    {
+        [Benchmark(Baseline = true)]
+        public void ClassicFizzBuzz()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = Implementations.ClassicFizzBuzz(i);
+            }
+        }
+
+        [Benchmark]
+        public void TuplesAndSwitchExpressions()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = Implementations.TuplesAndSwitchExpressions(i);
+            }
+        }
+        
+        [Benchmark]
+        public void SwitchExpressionsAndPatternMatching()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = Implementations.SwitchExpressionsAndPatternMatching(i);
+            }
+        }
+        
+        [Benchmark]
+        public void GetFizzBuzzFromAnswerFuncs()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = ArrayLookupFizzBuzz.GetFizzBuzzFromAnswerFuncs(i);
+            }
+        }
+        
+        [Benchmark]
+        public void GetFizzBuzzFromValueArrays()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = ArrayLookupFizzBuzz.GetFizzBuzzFromValueArrays(i);
+            }
+        }
+        
+        [Benchmark]
+        public void GetFizzBuzzFromValueHashsets()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                _ = ArrayLookupFizzBuzz.GetFizzBuzzFromValueHashsets(i);
+            }
+        }
+    }
+}
+
+#pragma warning restore CA1822
---
+++ src/FizzBuzz.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using Dgt.Dojo.FizzBuzz;
+
+_ = BenchmarkRunner.Run<FizzBuzzBenchmarks>();
