d70c2e3a049cb3da4df062a3536887bda48e5ad1
Mark Roberts <mmmcaffeine@gmail.com>
2022-10-22 17:45:58 +0100

    Used more appropriate data types
    * The specification stated a non-negative integer so switching from `int` to `uint` means we have to deal with larger values
    * We know we can only return a maximum value of 9 so we don't need the storage of an `int`

--- src/AddDigits.Tests/DigitsAdderTests.cs
+++ src/AddDigits.Tests/DigitsAdderTests.cs
@@ -3,11 +3,15 @@ namespace Dgt.Dojo.AddDigits;
 public class DigitsAdderTests
 {
     [Theory]
+    [InlineData(0, 0)]
     [InlineData(38, 2)]
     [InlineData(1337, 5)]
     [InlineData(987654321, 9)]
     [InlineData(1999999999, 1)]
     [InlineData(int.MaxValue, 1)]
-    public void AddDigits_Should_RecursivelyAddDigits(int value, int expected) =>
+    [InlineData(3999999999, 3)]
+    [InlineData(4199999999, 5)]
+    [InlineData(uint.MaxValue, 3)]
+    public void AddDigits_Should_RecursivelyAddDigits(uint value, byte expected) =>
         DigitsAdder.AddDigits(value).Should().Be(expected);
 }
--- src/AddDigits/DigitsAdder.cs
+++ src/AddDigits/DigitsAdder.cs
@@ -2,16 +2,16 @@
 
 public static class DigitsAdder
 {
-    public static int AddDigits(int value)
+    public static byte AddDigits(uint value)
     {
         var sum = value
             .ToString()
             .ToCharArray()
-            .Select(c => (int)char.GetNumericValue(c)).Sum(x => x);
+            .Select(c => (uint)char.GetNumericValue(c)).Sum(x => x);
 
         // Tail recursion... Will this be inefficient on very large numbers? Unlikely as with int we can't get up
-        // to three digits on the first iteration. The highest value we can get is 82. This limits the sum. I _think_
+        // to three digits on the first iteration. The highest value we can get is 84. This limits the sum. I _think_
         // we will need at most three iterations, so extra stack frames is probably not a concern to us
-        return sum >= 10 ? AddDigits(sum) : sum;
+        return sum >= 10 ? AddDigits((uint)sum) : (byte)sum;
     }
 }
